cf6d9fc43545c9d6d7748297d06178850411d5b4
jayaprakash <jnaguboina@gmail.com>
2017-09-17 23:04:36 -0400

    More commits

--- ims1.py
+++ ims1.py
@@ -1,10 +1,25 @@
 #Testing the code 
 
 
-list = ["Hello", "World"]
-print ("list %s") %list
 
-dict = {'hello':'value'}
-print (" i am printing dict %s") %dict[hello]
-# print dict[hello]
+newList = []
+print ("%r")%newList
+newList.append(1)
+newList.insert(5, 30)
+
+
+print ("%r")%newList
+newList.pop()
+newList.pop(0)
+print ("%r")%newList
+
+newList = [2,13,4,15,6,27,18,69]
+newList.sort()
+print ("%r")%newList
+newList.reverse()
+print ("%r")%newList
+
+
+newList = [2,13,4,15,6,27,18,69]
+print("r")%newList.index(4)
 
---
+++ ironman_py_scripts52.py~
@@ -0,0 +1,14 @@
+#!/bin/bash/python
+#Author - IronMan 
+# September 16 2017, time 4:51am
+
+
+class Apples(object):
+	
+	def apples(self):
+		print "Apples:"
+
+a = self.Apples()
+print ('a')
+
+
